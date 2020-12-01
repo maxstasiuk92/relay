99218e31bba391f8a92a7ba04912b5afc9ad6f8f
Maksym Stasiuk <maxstasiuk92@gmail.com>
2020-12-01 08:00:25 +0200

    try

--- src/main/java/maxstasiuk/relay/RelayApplication.java
+++ src/main/java/maxstasiuk/relay/RelayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class RelayApplication  {
-	
+	//some comment
 	public static void main(String[] args) {
 		SpringApplication.run(RelayApplication.class, args);
 	}
